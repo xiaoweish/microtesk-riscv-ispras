84ed14864dd31a021a1d85d6a0154d3fedf369d4
Protsenko <protsenko@intra.ispras.ru>
2016-11-04 12:35:37 +0300

    Specification was updated.
    Signed-off-by: Protsenko <protsenko@intra.ispras.ru>

--- microtesk-riscv/src/main/arch/riscv/templates/instruction_alu.rb
+++ microtesk-riscv/src/main/arch/riscv/templates/instruction_alu.rb
@@ -26,15 +26,7 @@ class InstructionALU < RISCVBaseTemplate
 
     addi r1, r0, 0x11
     trace "(add): r1 = %x", gpr_observer(1)
-    add r2, r1, r1
-    trace "(add): r2 = %x", gpr_observer(2)
-    addi r3, r0, 0x1
-    trace "(add): r3 = %x", gpr_observer(3)
 
-    ori r4, r2, 0x11
-    trace "(add): r4 = %x", gpr_observer(4)
-    add_d r5, r4, r4
-    trace "(add): r5 = %x", gpr_observer(5)
     nop
     nop
   end

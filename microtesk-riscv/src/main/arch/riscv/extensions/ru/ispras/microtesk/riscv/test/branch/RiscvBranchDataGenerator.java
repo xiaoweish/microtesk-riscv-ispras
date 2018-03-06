850abdf478e1a9af46cae9aee41099628f723fc0
Andrei Tatarnikov <andrewt@ispras.ru>
2018-03-06 14:46:20 +0300

    Added new branch data generators.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- microtesk-riscv/src/main/arch/riscv/extensions/ru/ispras/microtesk/riscv/test/branch/RiscvBranchDataGenerator.java
+++ microtesk-riscv/src/main/arch/riscv/extensions/ru/ispras/microtesk/riscv/test/branch/RiscvBranchDataGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 ISP RAS (http://www.ispras.ru)
+ * Copyright 2018 ISP RAS (http://www.ispras.ru)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -14,9 +14,105 @@
 
 package ru.ispras.microtesk.riscv.test.branch;
 
+import ru.ispras.fortress.data.types.bitvector.BitVector;
+import ru.ispras.fortress.expression.ExprUtils;
+import ru.ispras.fortress.expression.Node;
+import ru.ispras.fortress.expression.NodeValue;
+import ru.ispras.fortress.expression.NodeVariable;
+import ru.ispras.fortress.randomizer.Randomizer;
+import ru.ispras.fortress.util.InvariantChecks;
+import ru.ispras.fortress.util.Pair;
+import ru.ispras.microtesk.test.engine.branch.BranchDataGenerator;
+import ru.ispras.testbase.TestBaseContext;
+import ru.ispras.testbase.TestBaseQuery;
+import ru.ispras.testbase.TestData;
+import ru.ispras.testbase.knowledge.iterator.Iterator;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * {@link RiscvBranchDataGenerator} is a base class for the RISC-V branch instructions' generators.
+ *
+ * @author <a href="mailto:andrewt@ispras.ru">Andrei Tatarnikov</a>
  */
-public abstract class RiscvBranchDataGenerator {
-  // TODO
+public abstract class RiscvBranchDataGenerator extends BranchDataGenerator {
+  protected static Long generateEqual(Long rs1, Long rs2) {
+    if (rs1 == null) {
+      rs1 = rs2;
+    } else if (rs2 == null) {
+      rs2 = rs1;
+    } else {
+      InvariantChecks.checkTrue(rs1.equals(rs2), "Incorrect values defined");
+    }
+    if (rs1 == null) {
+      return Randomizer.get().nextLong();
+    }
+    return rs1;
+  }
+
+  protected static Pair<Long, Long> generateDistinct(Long rs1, Long rs2) {
+    if (rs1 == null && rs2 == null) {
+      rs1 = Randomizer.get().nextLong();
+      rs2 = distinctValue(rs1);
+    } else if (rs1 == null) {
+      rs1 = distinctValue(rs2);
+    } else if (rs2 == null) {
+      rs2 = distinctValue(rs1);
+    } else {
+      InvariantChecks.checkFalse(rs1.equals(rs2), "Incorrect values defined");
+    }
+
+    return new Pair<>(rs1, rs2);
+  }
+
+  private static long distinctValue(final long x) {
+    long value = x;
+    do {
+      value = Randomizer.get().nextLong();
+    } while (value == x);
+    return value;
+  }
+
+  protected static Iterator<TestData> generate(final TestBaseQuery query, final long rs) {
+    final String op = getInstructionName(query);
+    return generate(query, Collections.singletonMap(op + ".rs", rs));
+  }
+
+  protected static Long getValue(final String name, final TestBaseQuery query) {
+    final String op = getInstructionName(query);
+    final Node node = query.getBindings().get(op + "." + name);
+
+    InvariantChecks.checkNotNull(node, name);
+    InvariantChecks.checkTrue(ExprUtils.isVariable(node) || ExprUtils.isValue(node), name);
+
+    if (ExprUtils.isValue(node)) {
+      final NodeValue value = (NodeValue) node;
+      return value.getBitVector().longValue();
+    }
+
+    final NodeVariable var = (NodeVariable) node;
+    if (var.getData().hasValue()) {
+      return var.getData().getValue(BitVector.class).longValue();
+    }
+
+    return null;
+  }
+
+  protected static Iterator<TestData> generate(
+      final TestBaseQuery query,
+      final long rs,
+      final long rt) {
+    final String op  = getInstructionName(query);
+    final Map<String, Long> values = new HashMap<>();
+    values.put(op + ".rs1", rs);
+    values.put(op + ".rs2", rt);
+
+    return generate(query, values);
+  }
+
+  private static String getInstructionName(final TestBaseQuery query) {
+    return query.getContext().get(TestBaseContext.INSTRUCTION).toString();
+  }
 }

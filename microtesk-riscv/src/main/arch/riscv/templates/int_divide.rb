55e5c819cf61caf9137910963e18233d4a422f64
Andrei Tatarnikov <andrewt@ispras.ru>
2018-03-21 16:45:43 +0300

    int_divide.rb updated.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- microtesk-riscv/src/main/arch/riscv/templates/int_divide.rb
+++ microtesk-riscv/src/main/arch/riscv/templates/int_divide.rb
@@ -1,12 +1,17 @@
 #
-# MicroTESK for RISC-V
+# Copyright 2017-2018 ISP RAS (http://www.ispras.ru)
 #
-# Copyright (c) 2017 Institute for System Programming of the Russian Academy of Sciences
-# All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Institute for System Programming of the Russian Academy of Sciences (ISP RAS)
-# 25 Alexander Solzhenitsyn st., Moscow, 109004, Russia
-# http://www.ispras.ru
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 require_relative 'riscv_base'
@@ -23,8 +28,6 @@ require_relative 'riscv_base'
 class IntDivideTemplate < RISCVBaseTemplate
 
   def run
-    trace "Division: Debug Output"
-
     dividend = rand(0, 1023)
     divisor  = rand(1, 63) #zero is excluded
 
@@ -45,16 +48,14 @@ class IntDivideTemplate < RISCVBaseTemplate
     slt t3, t2, zero
 
     bne t3, zero, :done
-    nop
 
     add t1, zero, t2
     addi t0, t0, 1
 
     j :cycle
-    nop
 
     label :done
-    trace "\nCurrent register values: x5(t0) = %d, x6(t1) = %d\n",
+    trace "\nResult: quotient x5(t0) = %d, remainder x6(t1) = %d\n",
       gpr_observer(5), gpr_observer(6)
   end
 

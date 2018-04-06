4968140d879e7008d6d92862a8bf6ff438e3105a
Sergey Smolov <smolov@ispras.ru>
2018-04-06 15:02:49 +0300

    fixup
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- microtesk-riscv/src/test/java/ru/ispras/microtesk/model/riscv/RiscVTest.java
+++ microtesk-riscv/src/test/java/ru/ispras/microtesk/model/riscv/RiscVTest.java
@@ -562,12 +562,12 @@ public class RiscVTest extends TemplateTest {
 
         final byte [] errLogBytes = Files.readAllBytes(Paths.get(errorLog.getPath()));
         final String errString = new String(errLogBytes, Charset.defaultCharset());
-        final String terminationRegEx = "^.+: terminating on signal \\d+ from pid \\d+ \\(.+\\)$";
+        final String terminationRegEx = "^.+: terminating on signal \\d+ from pid \\d+ \\(.+\\)";
 
         if (!errString.matches(terminationRegEx)) {
           Assert.fail(
               String.format(
-                  "Process has returned %d: %s;%s%s Error log is:%s%s",
+                  "Process has returned %d: %s;%s%sError log is:%s%s",
                   exitCode,
                   Arrays.toString(cmdArray),
                   System.lineSeparator(),

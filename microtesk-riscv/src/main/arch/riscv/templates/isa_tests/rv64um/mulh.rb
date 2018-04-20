c1529ea7cf2a370ca5c32581617d8b7b7f64e278
Daniil <daniil.chuykin@phystech.edu>
2018-04-21 02:07:43 +0300

    rv64um/mul tests fixed.
    Signed-off-by: Daniil <daniil.chuykin@phystech.edu>

--- microtesk-riscv/src/main/arch/riscv/templates/isa_tests/rv64um/mulh.rb
+++ microtesk-riscv/src/main/arch/riscv/templates/isa_tests/rv64um/mulh.rb
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 # THIS FILE IS BASED ON THE FOLLOWING RISC-V TEST SUITE SOURCE FILE:
-# https://github.com/riscv/riscv-tests/blob/master/isa/rv64um/div.S
+# https://github.com/riscv/riscv-tests/blob/master/isa/rv64um/mulh.S
 # WHICH IS DISTRIBUTED UNDER THE FOLLOWING LICENSE:
 #
 # Copyright (c) 2012-2015, The Regents of the University of California (Regents).
@@ -56,53 +56,52 @@ class MulhTemplate < RISCVBaseTemplate
     # Arithmetic tests
     #-------------------------------------------------------------
 
-    TEST_RR_OP( 2,  'mulh', 0x00000000, 0x00000000, 0x00000000 );
-    TEST_RR_OP( 3,  'mulh', 0x00000000, 0x00000001, 0x00000001 );
-    TEST_RR_OP( 4,  'mulh', 0x00000000, 0x00000003, 0x00000007 );
+    TEST_RR_OP( 2,  'mulh', 0x00000000, 0x00000000, 0x00000000 )
+    TEST_RR_OP( 3,  'mulh', 0x00000000, 0x00000001, 0x00000001 )
+    TEST_RR_OP( 4,  'mulh', 0x00000000, 0x00000003, 0x00000007 )
 
-    TEST_RR_OP( 5,  'mulh', 0x0000000000000000, 0x0000000000000000, 0xffffffffffff8000 );
-    TEST_RR_OP( 6,  'mulh', 0x0000000000000000, 0xffffffff80000000, 0x00000000 );
-    TEST_RR_OP( 7,  'mulh', 0x0000000000000000, 0xffffffff80000000, 0xffffffffffff8000 );
+    TEST_RR_OP( 5,  'mulh', 0x0000000000000000, 0x0000000000000000, 0xffffffffffff8000 )
+    TEST_RR_OP( 6,  'mulh', 0x0000000000000000, 0xffffffff80000000, 0x00000000 )
+    TEST_RR_OP( 7,  'mulh', 0x0000000000000000, 0xffffffff80000000, 0xffffffffffff8000 )
 
     #-------------------------------------------------------------
     # Source/Destination tests
     #-------------------------------------------------------------
 
-    TEST_RR_SRC1_EQ_DEST( 8, 'mulh', 143, 13<<32, 11<<32 );
-    TEST_RR_SRC2_EQ_DEST( 9, 'mulh', 154, 14<<32, 11<<32 );
-    TEST_RR_SRC12_EQ_DEST( 10, 'mulh', 169, 13<<32 );
+    TEST_RR_SRC1_EQ_DEST( 8, 'mulh', 143, 13<<32, 11<<32 )
+    TEST_RR_SRC2_EQ_DEST( 9, 'mulh', 154, 14<<32, 11<<32 )
+    TEST_RR_SRC12_EQ_DEST( 10, 'mulh', 169, 13<<32 )
 
     #-------------------------------------------------------------
     # Bypassing tests
     #-------------------------------------------------------------
 
-    TEST_RR_DEST_BYPASS( 11, 0, 'mulh', 143, 13<<32, 11<<32 );
-    TEST_RR_DEST_BYPASS( 12, 1, 'mulh', 154, 14<<32, 11<<32 );
-    TEST_RR_DEST_BYPASS( 13, 2, 'mulh', 165, 15<<32, 11<<32 );
+    TEST_RR_DEST_BYPASS( 11, 0, 'mulh', 143, 13<<32, 11<<32 )
+    TEST_RR_DEST_BYPASS( 12, 1, 'mulh', 154, 14<<32, 11<<32 )
+    TEST_RR_DEST_BYPASS( 13, 2, 'mulh', 165, 15<<32, 11<<32 )
 
-    TEST_RR_SRC12_BYPASS( 14, 0, 0, 'mulh', 143, 13<<32, 11<<32 );
-    TEST_RR_SRC12_BYPASS( 15, 0, 1, 'mulh', 154, 14<<32, 11<<32 );
-    TEST_RR_SRC12_BYPASS( 16, 0, 2, 'mulh', 165, 15<<32, 11<<32 );
-    TEST_RR_SRC12_BYPASS( 17, 1, 0, 'mulh', 143, 13<<32, 11<<32 );
-    TEST_RR_SRC12_BYPASS( 18, 1, 1, 'mulh', 154, 14<<32, 11<<32 );
-    TEST_RR_SRC12_BYPASS( 19, 2, 0, 'mulh', 165, 15<<32, 11<<32 );
+    TEST_RR_SRC12_BYPASS( 14, 0, 0, 'mulh', 143, 13<<32, 11<<32 )
+    TEST_RR_SRC12_BYPASS( 15, 0, 1, 'mulh', 154, 14<<32, 11<<32 )
+    TEST_RR_SRC12_BYPASS( 16, 0, 2, 'mulh', 165, 15<<32, 11<<32 )
+    TEST_RR_SRC12_BYPASS( 17, 1, 0, 'mulh', 143, 13<<32, 11<<32 )
+    TEST_RR_SRC12_BYPASS( 18, 1, 1, 'mulh', 154, 14<<32, 11<<32 )
+    TEST_RR_SRC12_BYPASS( 19, 2, 0, 'mulh', 165, 15<<32, 11<<32 )
 
-    TEST_RR_SRC21_BYPASS( 20, 0, 0, 'mulh', 143, 13<<32, 11<<32 );
-    TEST_RR_SRC21_BYPASS( 21, 0, 1, 'mulh', 154, 14<<32, 11<<32 );
-    TEST_RR_SRC21_BYPASS( 22, 0, 2, 'mulh', 165, 15<<32, 11<<32 );
-    TEST_RR_SRC21_BYPASS( 23, 1, 0, 'mulh', 143, 13<<32, 11<<32 );
-    TEST_RR_SRC21_BYPASS( 24, 1, 1, 'mulh', 154, 14<<32, 11<<32 );
-    TEST_RR_SRC21_BYPASS( 25, 2, 0, 'mulh', 165, 15<<32, 11<<32 );
+    TEST_RR_SRC21_BYPASS( 20, 0, 0, 'mulh', 143, 13<<32, 11<<32 )
+    TEST_RR_SRC21_BYPASS( 21, 0, 1, 'mulh', 154, 14<<32, 11<<32 )
+    TEST_RR_SRC21_BYPASS( 22, 0, 2, 'mulh', 165, 15<<32, 11<<32 )
+    TEST_RR_SRC21_BYPASS( 23, 1, 0, 'mulh', 143, 13<<32, 11<<32 )
+    TEST_RR_SRC21_BYPASS( 24, 1, 1, 'mulh', 154, 14<<32, 11<<32 )
+    TEST_RR_SRC21_BYPASS( 25, 2, 0, 'mulh', 165, 15<<32, 11<<32 )
 
-    TEST_RR_ZEROSRC1( 26, 'mulh', 0, 31<<32 );
-    TEST_RR_ZEROSRC2( 27, 'mulh', 0, 32<<32 );
-    TEST_RR_ZEROSRC12( 28, 'mulh', 0 );
-    TEST_RR_ZERODEST( 29, 'mulh', 33<<32, 34<<32 );
+    TEST_RR_ZEROSRC1( 26, 'mulh', 0, 31<<32 )
+    TEST_RR_ZEROSRC2( 27, 'mulh', 0, 32<<32 )
+    TEST_RR_ZEROSRC12( 28, 'mulh', 0 )
+    TEST_RR_ZERODEST( 29, 'mulh', 33<<32, 34<<32 )
 
     TEST_PASSFAIL()
 
     RVTEST_CODE_END()
-
   
     RVTEST_DATA_BEGIN()
 
@@ -110,4 +109,5 @@ class MulhTemplate < RISCVBaseTemplate
 
     RVTEST_DATA_END()
   end
+
 end
--- microtesk-riscv/src/main/arch/riscv/templates/isa_tests/rv64um/mulw.rb
+++ microtesk-riscv/src/main/arch/riscv/templates/isa_tests/rv64um/mulw.rb
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 # THIS FILE IS BASED ON THE FOLLOWING RISC-V TEST SUITE SOURCE FILE:
-# https://github.com/riscv/riscv-tests/blob/master/isa/rv64um/div.S
+# https://github.com/riscv/riscv-tests/blob/master/isa/rv64um/mulw.S
 # WHICH IS DISTRIBUTED UNDER THE FOLLOWING LICENSE:
 #
 # Copyright (c) 2012-2015, The Regents of the University of California (Regents).
@@ -43,72 +43,71 @@
 # MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #
 require_relative '../../riscv_base'
+
 class MulwTemplate < RISCVBaseTemplate
+
   def pre_rvtest
     RVTEST_RV64U()
     RVTEST_CODE_BEGIN()
   end
 
   def run
-
     #-------------------------------------------------------------
     # Arithmetic tests
     #-------------------------------------------------------------
 
-    TEST_RR_OP( 2,  'mulw', 0x00000000, 0x00000000, 0x00000000 );
-    TEST_RR_OP( 3,  'mulw', 0x00000001, 0x00000001, 0x00000001 );
-    TEST_RR_OP( 4,  'mulw', 0x00000015, 0x00000003, 0x00000007 );
+    TEST_RR_OP( 2,  'mulw', 0x00000000, 0x00000000, 0x00000000 )
+    TEST_RR_OP( 3,  'mulw', 0x00000001, 0x00000001, 0x00000001 )
+    TEST_RR_OP( 4,  'mulw', 0x00000015, 0x00000003, 0x00000007 )
 
-    TEST_RR_OP( 5,  'mulw', 0x0000000000000000, 0x0000000000000000, 0xffffffffffff8000 );
-    TEST_RR_OP( 6,  'mulw', 0x0000000000000000, 0xffffffff80000000, 0x00000000 );
-    TEST_RR_OP( 7,  'mulw', 0x0000000000000000, 0xffffffff80000000, 0xffffffffffff8000 );
+    TEST_RR_OP( 5,  'mulw', 0x0000000000000000, 0x0000000000000000, 0xffffffffffff8000 )
+    TEST_RR_OP( 6,  'mulw', 0x0000000000000000, 0xffffffff80000000, 0x00000000 )
+    TEST_RR_OP( 7,  'mulw', 0x0000000000000000, 0xffffffff80000000, 0xffffffffffff8000 )
 
     #-------------------------------------------------------------
     # Source/Destination tests
     #-------------------------------------------------------------
 
-    TEST_RR_SRC1_EQ_DEST( 8, 'mulw', 143, 13, 11 );
-    TEST_RR_SRC2_EQ_DEST( 9, 'mulw', 154, 14, 11 );
-    TEST_RR_SRC12_EQ_DEST( 10, 'mulw', 169, 13 );
+    TEST_RR_SRC1_EQ_DEST( 8, 'mulw', 143, 13, 11 )
+    TEST_RR_SRC2_EQ_DEST( 9, 'mulw', 154, 14, 11 )
+    TEST_RR_SRC12_EQ_DEST( 10, 'mulw', 169, 13 )
 
     #-------------------------------------------------------------
     # Bypassing tests
     #-------------------------------------------------------------
 
-    TEST_RR_DEST_BYPASS( 11, 0, 'mulw', 143, 13, 11 );
-    TEST_RR_DEST_BYPASS( 12, 1, 'mulw', 154, 14, 11 );
-    TEST_RR_DEST_BYPASS( 13, 2, 'mulw', 165, 15, 11 );
+    TEST_RR_DEST_BYPASS( 11, 0, 'mulw', 143, 13, 11 )
+    TEST_RR_DEST_BYPASS( 12, 1, 'mulw', 154, 14, 11 )
+    TEST_RR_DEST_BYPASS( 13, 2, 'mulw', 165, 15, 11 )
 
-    TEST_RR_SRC12_BYPASS( 14, 0, 0, 'mulw', 143, 13, 11 );
-    TEST_RR_SRC12_BYPASS( 15, 0, 1, 'mulw', 154, 14, 11 );
-    TEST_RR_SRC12_BYPASS( 16, 0, 2, 'mulw', 165, 15, 11 );
-    TEST_RR_SRC12_BYPASS( 17, 1, 0, 'mulw', 143, 13, 11 );
-    TEST_RR_SRC12_BYPASS( 18, 1, 1, 'mulw', 154, 14, 11 );
-    TEST_RR_SRC12_BYPASS( 19, 2, 0, 'mulw', 165, 15, 11 );
+    TEST_RR_SRC12_BYPASS( 14, 0, 0, 'mulw', 143, 13, 11 )
+    TEST_RR_SRC12_BYPASS( 15, 0, 1, 'mulw', 154, 14, 11 )
+    TEST_RR_SRC12_BYPASS( 16, 0, 2, 'mulw', 165, 15, 11 )
+    TEST_RR_SRC12_BYPASS( 17, 1, 0, 'mulw', 143, 13, 11 )
+    TEST_RR_SRC12_BYPASS( 18, 1, 1, 'mulw', 154, 14, 11 )
+    TEST_RR_SRC12_BYPASS( 19, 2, 0, 'mulw', 165, 15, 11 )
 
-    TEST_RR_SRC21_BYPASS( 20, 0, 0, 'mulw', 143, 13, 11 );
-    TEST_RR_SRC21_BYPASS( 21, 0, 1, 'mulw', 154, 14, 11 );
-    TEST_RR_SRC21_BYPASS( 22, 0, 2, 'mulw', 165, 15, 11 );
-    TEST_RR_SRC21_BYPASS( 23, 1, 0, 'mulw', 143, 13, 11 );
-    TEST_RR_SRC21_BYPASS( 24, 1, 1, 'mulw', 154, 14, 11 );
-    TEST_RR_SRC21_BYPASS( 25, 2, 0, 'mulw', 165, 15, 11 );
+    TEST_RR_SRC21_BYPASS( 20, 0, 0, 'mulw', 143, 13, 11 )
+    TEST_RR_SRC21_BYPASS( 21, 0, 1, 'mulw', 154, 14, 11 )
+    TEST_RR_SRC21_BYPASS( 22, 0, 2, 'mulw', 165, 15, 11 )
+    TEST_RR_SRC21_BYPASS( 23, 1, 0, 'mulw', 143, 13, 11 )
+    TEST_RR_SRC21_BYPASS( 24, 1, 1, 'mulw', 154, 14, 11 )
+    TEST_RR_SRC21_BYPASS( 25, 2, 0, 'mulw', 165, 15, 11 )
 
-    TEST_RR_ZEROSRC1( 26, 'mulw', 0, 31 );
-    TEST_RR_ZEROSRC2( 27, 'mulw', 0, 32 );
-    TEST_RR_ZEROSRC12( 28, 'mulw', 0 );
-    TEST_RR_ZERODEST( 29, 'mulw', 33, 34 );
+    TEST_RR_ZEROSRC1( 26, 'mulw', 0, 31 )
+    TEST_RR_ZEROSRC2( 27, 'mulw', 0, 32 )
+    TEST_RR_ZEROSRC12( 28, 'mulw', 0 )
+    TEST_RR_ZERODEST( 29, 'mulw', 33, 34 )
 
     TEST_PASSFAIL()
 
     RVTEST_CODE_END()
 
-  
     RVTEST_DATA_BEGIN()
 
     TEST_DATA()
 
     RVTEST_DATA_END()
-
   end
 
 end

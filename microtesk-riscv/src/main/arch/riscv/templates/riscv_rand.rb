07c79ee12feaa6028b934556b00015180ee7e461
Andrei Tatarnikov <andrewt@ispras.ru>
2018-06-13 11:43:52 +0300

    riscv_rand.rb updated
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- microtesk-riscv/src/main/arch/riscv/templates/riscv_rand.rb
+++ microtesk-riscv/src/main/arch/riscv/templates/riscv_rand.rb
@@ -24,74 +24,68 @@
 #
 module RiscvRand
 
-  def rand_word  rand(0, 0xFFFF_FFFF) end
-  def rand_dword rand(0, 0xFFFF_FFFF_FFFF_FFFF) end
-
-  def rand_range(low, high)
-    rand(low, high)
+  def self.bit_set_values
+    array = []
+    (0..63).each { |n|
+      v = 1 << n
+      array << v
+    }
+    array
   end
 
-  def rand_shamt   rand_range(0, 63) end
-  def rand_shamtw  rand_range(0, 31) end
-  def rand_seglen  rand_range(0, 7) end
-  def rand_imm     rand_range(-2048, 2047) end
-  def rand_bigimm  rand_range(0, 1048575) end
+  def self.bit_clear_values
+    array = []
+    (0..63).each { |n|
+      v = 1 << n
+      array << ~v
+    }
+    array
+  end
 
-  def rand_addr_b(memsize) rand_range(0, memsize-1) end
-  def rand_addr_h(memsize) rand_range(0, memsize-1) & ~1 end
-  def rand_addr_w(memsize) rand_range(0, memsize-1) & ~3 end
-  def rand_addr_d(memsize) rand_range(0, memsize-1) & ~7 end
+  BIT_SET_VALUES = bit_set_values()
+  BIT_CLEAR_VALUES = bit_clear_values()
 
-#  def rand_biased
-#    rand(dist(
-#      range(:value => , :bias => ),
-#      range(:value => , :bias => ),
-#      range(:value => , :bias => ),
-#      range(:value => , :bias => ),
-#      range(:value => , :bias => ),
-#      range(:value => , :bias => )
-#    ))
-#  end
+  def rand_word;             rand(0x0, 0xFFFF_FFFF) end
+  def rand_dword;            rand(0x0, 0xFFFF_FFFF_FFFF_FFFF) end
+  def rand_range(low, high); rand(low, high) end
 
-#  def rand_biased: Long =
-#  {
-#    val value = rand_dword
-#    val s = rand_range(0, 17)
-#
-#    if (s < 9)
-#    {
-#      val small = rand_range(0, 9).toLong
-#
-#      s match
-#      {
-#        // return a value with a single bit set
-#        case 0 => (1 << value & 63)
-#        case 1 => (1 << value & 63)
-#
-#        // return a valueue with a single bit clear
-#        case 2 => ~(1 << value & 63)
-#        case 3 => ~(1 << value & 63)
-#
-#        // return a small integer around zero
-#        case 4 => small
-#
-#        // return a very large/very small 8b signed number
-#        case 5 => ((0x80L + small) << 56) >> 56
-#
-#        // return a very large/very small 16b signed number
-#        case 6 => ((0x8000L + small) << 48) >> 48
-#
-#        // return a very large/very small 32b signed number
-#        case 7 => ((0x80000000L + small) << 32) >> 32
-#
-#        // return a very large/very small 64b signed number
-#        case 8 => 0x800000000000000L + small
-#      }
-#    }
-#    else
-#    {
-#      value
-#    }
-#  }
+  def rand_shamt;  rand_range(0, 63) end
+  def rand_shamtw; rand_range(0, 31) end
+  def rand_seglen; rand_range(0, 7) end
+  def rand_imm;    rand_range(-2048, 2047) end
+  def rand_bigimm; rand_range(0, 1048575) end
+
+  def rand_addr_b(memsize); rand_range(0, memsize-1) end
+  def rand_addr_h(memsize); rand_range(0, memsize-1) & ~1 end
+  def rand_addr_w(memsize); rand_range(0, memsize-1) & ~3 end
+  def rand_addr_d(memsize); rand_range(0, memsize-1) & ~7 end
+
+  def rand_biased
+    rand(dist(
+      # 0-1: return a value with a single bit set
+      range(:value => BIT_SET_VALUES, :bias => 1110),
+
+      # 2-3: return a value with a single bit clear
+      range(:value => BIT_CLEAR_VALUES, :bias => 1110),
+
+      # 4: return a small integer around zero
+      range(:value => 0..9, :bias => 556),
+
+      # 5: return a very large/very small 8b signed number
+      range(:value => FFFF_FFFF_FFFF_FF80..0xFFFF_FFFF_FFFF_FF89, :bias => 556),
+
+      # 6: return a very large/very small 16b signed number
+      range(:value => 0xFFFF_FFFF_FFFF_8000..0xFFFF_FFFF_FFFF_8009, :bias => 556),
+
+      # 7: return a very large/very small 32b signed number
+      range(:value => 0xFFFF_FFFF_8000_0000..0xFFFF_FFFF_8000_0009, :bias => 556),
+
+      # 8: return a very large/very small 64b signed number
+      range(:value => 0x8000_0000_0000_0000..0x8000_0000_0000_0009, :bias => 556),
+
+      # 9-17: return a random dword value
+      range(:value => 0x0..0xFFFF_FFFF_FFFF_FFFF, :bias => 5000)
+    ))
+  end
 
 end

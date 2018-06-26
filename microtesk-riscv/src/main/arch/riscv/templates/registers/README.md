556f0a45dfffaee3b0d836221ac5f1c8eb01e92f
Andrei Tatarnikov <andrewt@ispras.ru>
2018-06-26 18:29:22 +0300

    [templates] Separate folder for register allocation templates.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

---
+++ microtesk-riscv/src/main/arch/riscv/templates/registers/README.md
@@ -0,0 +1,15 @@
+Register Test Templates
+=============================
+
+### About
+
+This folder contains test templates that demonstrate facilities for selecting instructions
+to be used by instructions.
+
+### Running Test Generation
+
+To generate test programs for the given test templates, simply run the following command:
+
+    make
+
+To generate test programs for a specific test template(s), pass its name(s) to the make command.
--- microtesk-riscv/src/test/java/ru/ispras/microtesk/model/riscv/registers/RegisterAllocationTestCase.java
+++ microtesk-riscv/src/test/java/ru/ispras/microtesk/model/riscv/registers/RegisterAllocationTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 ISP RAS (http://www.ispras.ru)
+ * Copyright 2017-2018 ISP RAS (http://www.ispras.ru)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -12,17 +12,18 @@
  * the License.
  */
 
-package ru.ispras.microtesk.model.riscv;
+package ru.ispras.microtesk.model.riscv.registers;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import ru.ispras.microtesk.model.riscv.RiscVTest;
 import ru.ispras.microtesk.test.Statistics;
 
 public class RegisterAllocationTestCase extends RiscVTest {
   @Test
   public void test() {
-    final Statistics statistics = run("register_allocation.rb");
+    final Statistics statistics = run("registers/register_allocation.rb");
     Assert.assertNotNull(statistics);
   }
 }

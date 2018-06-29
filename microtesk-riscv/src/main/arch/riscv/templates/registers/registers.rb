c48114f7d279354899e083e5030854aeff8032db
Andrei Tatarnikov <andrewt@ispras.ru>
2018-06-29 12:19:56 +0300

    registers/registers.rb updated.
    Signed-off-by: Andrei Tatarnikov <andrewt@ispras.ru>

--- microtesk-riscv/src/main/arch/riscv/templates/registers/registers.rb
+++ microtesk-riscv/src/main/arch/riscv/templates/registers/registers.rb
@@ -28,27 +28,27 @@ class RegistersTemplate < RISCVBaseTemplate
     sequence {
       add x7, x5, x6
       sub x9, x7, x8
-    }
+    }.run
 
     # Also, they can be accessed by their aliases. For example:
     sequence {
       add t2, t0, t1
       sub s1, t2, s0
-    }
+    }.run
 
     # It is possible to specify them by their numbers. This is useful when it is required
     # to parametrise register numbers with specific data. For example:
     sequence {
       add x(7), x(5), x(6)
       sub x(9), x(7), x(8)
-    }
+    }.run
 
     # Registers numbers can be specified as random values. In the example below, register
     # shared between the two instructions is specified using the 'r' variable.
     sequence {
       add r=x(rand(0, 31)), x(rand(0, 31)), x(rand(0, 31))
       sub x(rand(0, 31)), r, x(rand(0, 31))
-    }
+    }.run
 
     # Registers can be selected automatically using a configurable set of register allocation
     # strategies. In the simplest case like the one below, it is very similar to random selection.
@@ -57,7 +57,7 @@ class RegistersTemplate < RISCVBaseTemplate
     sequence {
       add r=x(_), x(_), x(_)
       sub x(_), r, x(_)
-    }
+    }.run
   end
 
 end

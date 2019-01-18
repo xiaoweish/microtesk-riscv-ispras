a5a2fd14f88104d577e71923950812a68c86d125
Sergey Smolov <smolov@ispras.ru>
2019-01-18 11:33:52 +0300

    junit: rm unused import
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- microtesk-riscv/src/test/java/ru/ispras/microtesk/model/riscv/RiscVTest.java
+++ microtesk-riscv/src/test/java/ru/ispras/microtesk/model/riscv/RiscVTest.java
@@ -35,7 +35,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashSet;

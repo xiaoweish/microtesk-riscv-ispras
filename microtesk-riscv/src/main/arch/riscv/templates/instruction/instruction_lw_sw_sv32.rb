3bcb7a101a9433d3dc2d7a0a5ee9f56bd63063ec
Protsenko <protsenko@ispras.ru>
2019-01-30 11:52:34 +0300

    upd: template for sv32
    Signed-off-by: Protsenko <protsenko@ispras.ru>

--- microtesk-riscv/src/main/arch/riscv/templates/instruction/instruction_lw_sw_sv32.rb
+++ microtesk-riscv/src/main/arch/riscv/templates/instruction/instruction_lw_sw_sv32.rb
@@ -23,34 +23,40 @@ require_relative '../riscv_base'
 class InstructionMemTemplate < RiscVBaseTemplate
 
   def TEST_DATA
-    data {
-      org 0x00010000
-      label :data
-      word 0x0, 0x0, 0x0, 0x0,
-           0x0, 0x0, 0x0, 0x0
-      label :end
-      space 1
+    section(:name => '.page_table_sv32_step1',
+            :pa   => 0x00000000BED22800,
+            :va   => 0x00000000BED22800) {
+      data {
+        # Page Table Level: 1
+        label :data1
+        word 0x33B488e1, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef
+        label :end1
+        space 1
+      }
+    }
 
-      # Page Table Level: 1
-      org 0x3ed00800
-      label :data1
-      word 0x33B488e1, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef
-      label :end1
-      space 1
+    section(:name => '.page_table_sv32_step0',
+            :pa   => 0x00000000CED22040,
+            :va   => 0x00000000CED22040) {
+      data {
+        # Page Table Level: 0
+        label :data0
+        word 0x37B488e3, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef
+        label :end0
+        space 1
+      }
+    }
 
-      # Page Table Level: 0
-      org 0x4ed000c8
-      label :data2
-      word 0x37B488e3, 0xdeadbeef, 0xdeadbeef, 0xdeadbeef
-      label :end2
-      space 1
-
-      # Data
-      org 0x5ed00000
-      label :data3
-      word 0xc001beef, 0xc001beef, 0xc001beef, 0xc001beef
-      label :end3
-      space 1
+    section(:name => '.data_for_sv32',
+            :pa   => 0x00000000DED22130,
+            :va   => 0x00000000DED22130) {
+      data {
+        # Data
+        label :data
+        word 0xc001beef, 0xc001beef, 0xc001beef, 0xc001beef
+        label :end
+        space 1
+      }
     }
   end
 
@@ -58,14 +64,12 @@ class InstructionMemTemplate < RiscVBaseTemplate
     # Only for Sv32, (RV32)
     # csrwi satp, 0x80000002 # MODE = 1, PPN = 0x2
     trace "satp = 0x%x", satp
-    li t1, :data1
-    trace "t1 = 0x%x", t1
     li t0, 0x800bed22
     trace "t0 = 0x%x", t0
     csrw satp, t0
     trace "satp = 0x%x", satp
 
-    la s0, :data # Address
+    la s0, 0x00010000 # Address
     prepare t0, 0xFFFFFFFFDEADBEEF # Value being loaded/stored
 
     trace "s0 = 0x%x", s0

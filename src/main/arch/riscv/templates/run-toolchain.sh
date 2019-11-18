f4112aad1d7c3ba7aae879f3182ba4371fc4ff52
Sergey Smolov <smolov@ispras.ru>
2019-11-18 15:04:17 +0300

    run-toolchain.sh: compilation flag imafdc -> gcv
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- src/main/arch/riscv/templates/run-toolchain.sh
+++ src/main/arch/riscv/templates/run-toolchain.sh
@@ -6,7 +6,7 @@ out_dir="$MICROTESK_HOME/output/${cur_dir##*arch/riscv/templates}/$1"
 if [ -x "$(command -v riscv64-unknown-elf-gcc)" ] &&
    [ -x "$(command -v riscv64-unknown-elf-objdump)" ] ; then
   for file in $out_dir/$1*.s; do
-    riscv64-unknown-elf-gcc -nostdlib -nostartfiles -Wa,-march=rv64imafdc \
+    riscv64-unknown-elf-gcc -nostdlib -nostartfiles -Wa,-march=rv64gcv \
       -T$out_dir/$1.ld $file -o ${file%%.*}.elf
     riscv64-unknown-elf-objdump ${file%%.*}.elf --disassemble-all --disassemble-zeroes \
       --section=.text --section=.text.startup --section=.text.init --section=.data\

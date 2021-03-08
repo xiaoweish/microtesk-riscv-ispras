3f5222f3bf758ff259b201be9c3e71c9e5bfcd18
Sergey Smolov <smolov@ispras.ru>
2021-03-08 15:08:55 +0300

    test: RiscVTest is abstract
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- src/test/java/ru/ispras/microtesk/model/riscv/RiscVTest.java
+++ src/test/java/ru/ispras/microtesk/model/riscv/RiscVTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 ISP RAS (http://www.ispras.ru)
+ * Copyright 2017-2021 ISP RAS (http://www.ispras.ru)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -34,7 +34,7 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 
-public class RiscVTest extends TemplateTest {
+public abstract class RiscVTest extends TemplateTest {
 
   /**
    * The test program name prefix.
